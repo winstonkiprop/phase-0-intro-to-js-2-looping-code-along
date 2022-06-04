2129aefee3f4090d954c6c318b35166c6adda319
winston kiprop <winstonekip50@gmail.com>
2022-06-04 18:37:06 +0300

    commit tree a5e5cca4c708501c09488567f50dad01b37f9c57

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+let emptyArray = [];
+function writeCards(cards){
+    for (let i = 0; i < cards.length; i++){
+        emptyArray.push(`Thank you, ${cards[i]}, for the wonderful surprise gift!`);
+    }
+    return emptyArray
+}
+
+function countDown(integer) {
+    while (integer >= 0) {
+        console.log(integer);
+        integer--;
+    }
+}
+countDown(8);
+   
